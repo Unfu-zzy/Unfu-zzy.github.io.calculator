cea81e04a9462af7fafda8d531404219872d3ffb
Unfu-zzy <89948660+Unfu-zzy@users.noreply.github.com>
2021-11-10 16:43:17 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,11 @@
+function number(num){
+document.form1.textview.value = document.form1.textview.value + num;
+}
+
+function equal(){
+var exp = document.form1.textview.value;
+if(exp){
+document.form1.textview.value = eval(exp)
+}
+}
+
